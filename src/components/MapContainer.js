389ab4dd6adb1a500743dbdae20e440ba81fbbae
Jason Stevens <jason.stevens@shaw.ca>
2018-12-20 11:27:47 -0700

    Progress update

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -15,6 +15,7 @@ class MainMap extends Component {
   render() {
     const position = [32, -95];
     const position2 = [-4.341, -145.49];
+    const eclipseLine = [];
 
     return (
       <Consumer>
@@ -30,12 +31,33 @@ class MainMap extends Component {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | 2024 Eclipse Planner'
                   />
+                  {eclipsePath.map(point => {
+                    eclipseLine.push([point[1], point[2]]);
+                  })}
 
-                  {eclipsePath.map(point => (
-                    <Marker key={point[0]} position={[point[1], point[2]]}>
-                      {point[0]}
-                    </Marker>
-                  ))}
+                  <Polyline
+                    positions={eclipseLine}
+                    color="yellow"
+                    weight="100"
+                    opacity=".1"
+                    lineCap="round"
+                  />
+
+                  <Polyline
+                    positions={eclipseLine}
+                    color="orange"
+                    weight="50"
+                    opacity=".2"
+                    lineCap="round"
+                  />
+
+                  <Polyline
+                    positions={eclipseLine}
+                    color="red"
+                    weight="10"
+                    opacity=".3"
+                    lineCap="round"
+                  />
                 </Map>
               </React.Fragment>
             );
